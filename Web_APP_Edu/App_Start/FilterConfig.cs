7806a817ad9d8bd1424356d1eb1b57924b7050aa
aashishshar <aashishshar@gmail.com>
2017-06-25 16:00:05 +0530

    ui

---
+++ Web_APP_Edu/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Web_APP_Edu
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Web_APP_Edu/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web_APP_Edu.Startup))]
+namespace Web_APP_Edu
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
